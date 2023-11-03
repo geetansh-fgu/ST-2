299752447bdfac5eade42243901d2118d2984b7d
Geetansh <123066928+geetansh-fgu@users.noreply.github.com>
2023-11-03 21:50:40 +0530

    Update steps.cpp

--- steps.cpp
+++ steps.cpp
@@ -1,3 +1,6 @@
+// Q.There are n stairs, a person standing at the bottom wants to climb stairs to reach the nth stair.
+//The person can climb atmost m stairs at a time, the task is to count the number of ways that a person can reach at the top.
+
 #include<iostream>
 using namespace std;
 int helper(int n, int &m){
